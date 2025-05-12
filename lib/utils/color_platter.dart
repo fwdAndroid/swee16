27127b1416edb1c8ef5ffd92d1e647a65b848c8d
fwdAndroid <fwdkaleem@gmail.com>
2025-05-12 10:17:27 +0500

    Colors Platte

---
+++ lib/utils/color_platter.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final mainColor = const Color(0xFF90EE90);
+final blackColor = Colors.black;
+
+final buttonColor = const Color(0xFF00AB66);
+final removeColor = Colors.red;
+final whiteColor = Colors.white;
+final textFieldColor = const Color(0xFF200E32);
