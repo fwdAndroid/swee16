8662776324d71187f7e182abc1b8b5b6194a3888
fwdAndroid <fwdkaleem@gmail.com>
2025-05-15 00:07:28 +0500

    firebase intialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:swee16/firebase_options.dart';
 import 'package:swee16/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
