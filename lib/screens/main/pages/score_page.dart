afcce65fae788ac63b41873e55251eac9b253460
fwdAndroid <fwdkaleem@gmail.com>
2025-05-19 11:06:05 +0500

    Show and Delete Practice

--- lib/screens/main/pages/score_page.dart
+++ lib/screens/main/pages/score_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:swee16/utils/color_platter.dart';
 import 'package:swee16/widget/numberWidget.dart';
 
@@ -10,163 +12,158 @@ class ScorePage extends StatefulWidget {
 }
 
 class _ScorePageState extends State<ScorePage> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  List<QueryDocumentSnapshot> _practiceSessions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPracticeSessions();
+  }
+
+  void _loadPracticeSessions() {
+    _firestore
+        .collection('practice_sessions')
+        .orderBy('timestamp', descending: true)
+        .snapshots()
+        .listen((snapshot) {
+          setState(() {
+            _practiceSessions = snapshot.docs;
+          });
+        });
+  }
+
+  void _deleteSession(String sessionId) async {
+    await _firestore.collection('practice_sessions').doc(sessionId).delete();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-
       appBar: AppBar(
         backgroundColor: blackColor,
-
         automaticallyImplyLeading: false,
         title: Text("Fawad Kaleem", style: TextStyle(color: whiteColor)),
       ),
-      body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      height: 160,
-                      width: 280,
-                      child: Card(
-                        color: textFieldColor,
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Align(
-                                alignment: Alignment.topRight,
-                                child: Text(
-                                  "23 December 2023",
-                                  style: TextStyle(
-                                    color: whiteColor,
-                                    fontSize: 12,
-                                  ),
+      body:
+          _practiceSessions.isEmpty
+              ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.hourglass_empty, size: 64, color: whiteColor),
+                    SizedBox(height: 16),
+                    Text(
+                      "No practice sessions found",
+                      style: TextStyle(color: whiteColor, fontSize: 18),
+                    ),
+                  ],
+                ),
+              )
+              : ListView.builder(
+                itemCount: _practiceSessions.length,
+                itemBuilder: (context, index) {
+                  final session = _practiceSessions[index];
+                  final date = DateFormat(
+                    'dd MMMM y',
+                  ).format(session['timestamp'].toDate());
+
+                  return Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              height: 160,
+                              width: 280,
+                              child: Card(
+                                color: textFieldColor,
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Align(
+                                        alignment: Alignment.topRight,
+                                        child: Text(
+                                          date,
+                                          style: TextStyle(
+                                            color: whiteColor,
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    // Numbers 1-8
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: List.generate(8, (i) {
+                                          final number = i + 1;
+                                          final sessionData =
+                                              session.data()
+                                                  as Map<String, dynamic>;
+                                          final percentage =
+                                              sessionData.containsKey('$number')
+                                                  ? (sessionData['$number']['percentage']
+                                                          ?.toString() ??
+                                                      '0')
+                                                  : '0';
+
+                                          return Numberwidget(
+                                            title: "$number",
+                                            color: getNumberColor(number),
+                                            number: "$percentage%",
+                                          );
+                                        }),
+                                      ),
+                                    ),
+                                    // Numbers 9-16
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: List.generate(8, (i) {
+                                          final number = i + 9;
+                                          final sessionData =
+                                              session.data()
+                                                  as Map<String, dynamic>;
+                                          final percentage =
+                                              sessionData.containsKey('$number')
+                                                  ? (sessionData['$number']['percentage']
+                                                          ?.toString() ??
+                                                      '0')
+                                                  : '0';
+
+                                          return Numberwidget(
+                                            title: "$number",
+                                            color: getNumberColor(number),
+                                            number: "$percentage%",
+                                          );
+                                        }),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
-
-                            //1 to 8
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Numberwidget(
-                                    title: "1",
-                                    color: blueLight,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "2",
-                                    color: lightGreen,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "3",
-                                    color: brightNeonGreen,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "4",
-                                    color: vivedYellow,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "5",
-                                    color: brownishOrange,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "6",
-                                    color: hotPink,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "7",
-                                    color: oliveGreen,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "8",
-                                    color: goldenOrange,
-                                    number: "1%",
-                                  ),
-                                ],
-                              ),
-                            ),
-                            //9 to 16
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Numberwidget(
-                                    title: "9",
-                                    color: red,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "10",
-                                    color: goldenYellow,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "11",
-                                    color: lightGrey,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "12",
-                                    color: purpleBlue,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "13",
-                                    color: warmOrange,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "14",
-                                    color: royalPurple,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "15",
-                                    color: greenishGrey,
-                                    number: "1%",
-                                  ),
-                                  Numberwidget(
-                                    title: "16",
-                                    color: margintaPink,
-                                    number: "1%",
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                          IconButton(
+                            onPressed: () => _deleteSession(session.id),
+                            icon: Icon(Icons.delete, color: removeColor),
+                          ),
+                        ],
                       ),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.delete, color: removeColor),
-                  ),
-                ],
+                      Divider(color: whiteColor, height: 3),
+                    ],
+                  );
+                },
               ),
-              Divider(color: whiteColor, height: 3),
-            ],
-          );
-        },
-      ),
     );
   }
 }
+ 
